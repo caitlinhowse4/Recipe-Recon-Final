c0c4f83dee1075a4ab6c74538a26e5ec2ee7a8dd
Lau300 <204911059+Lau300@users.noreply.github.com>
2025-05-06 11:31:02 +1200

    Created a suggestion forum based on framework
    Can input a suggestion and the suggestion is displayed. The current forum is not linked to the database. Changed the App.js in order to display the forum. Only displays the suggestion forum.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TodoForm from './TodoForm';
+import SuggestionForum from './pages/SuggestionForum';
 import './styles/App.css';
 
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the Express server
-    axios.get('http://localhost:5000/todos')
-      .then(response => setTodos(response.data))
-      .catch(error => console.error(error));
-  }, []);
-
-const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
-
-
-  return (
-    <div className="container">
-      <h1>Recipe recalculator app</h1>
-      <TodoForm onAdd={addTodo} />
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo._id}>{todo.task}</li>
-        ))}
-      </ul>
-    </div>
-  );
+       //uncomment all below if using database
+       // const [suggestion, setSuggestion] = useState([]);
+       // useEffect(() => {
+       //     axios.get(`https://localhost:5000/suggestion`)
+       //         .then(response => setSuggestion(response.data))
+       //         .catch(error => console.error(error));
+       //     },[]);
+        return (
+            <div className="container">
+                <h1>Suggestion Forum</h1>
+                <SuggestionForum/>
+            </div>//if using database - <SuggestionForum suggestion={suggestion} setSuggestion={setSuggestion}/>
+        );
 };
 export default App;
 
