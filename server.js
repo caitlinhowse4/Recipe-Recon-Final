58e8b2058c2cb48499bfd64a67d4a68539d8348a
Feba-sara <204156767+Feba-sara@users.noreply.github.com>
2025-05-06 19:19:29 +1200

    commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
 app.use(cors());
 app.use(express.json());
 
