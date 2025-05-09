d93065e7a54656e977e9c3fbe71336f55b20317d
Lau300 <204911059+Lau300@users.noreply.github.com>
2025-05-09 22:17:02 +1200

    changed suggestion forum back to original code
    I couldn't figure out how to save the previous code of it into local storage. I've also changed the files in the previous commit to local storage as I've been unable to connect to the database.

--- src/SuggestionForum.js
+++ src/SuggestionForum.js
@@ -1,32 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const SuggestionForum = () => {
-    const [suggestion, setSuggestion] = useState('');
-    const [suggestionSaved, setSuggestionsSaved] = useState([]);
-
-    useEffect(() => {
-        const savedSuggestions = JSON.parse(localStorage.getItem('savedSuggestions')) || [];
-        setSuggestionsSaved(savedSuggestions);
-    },[]);
-    
-    const saveSuggest = async () => {
-        const newSuggest = {name: suggestion};
-        const newSuggestion = [...suggestionSaved, newSuggest];
-        setSuggestionsSaved(newSuggestion);
-        localStorage.setItem('savedSuggestions', JSON.stringify(newSuggestion));
-
+    const [suggestion, setSuggestion] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newSuggestion = e.target.suggestion.value.trim();
+        if(newSuggestion){
+            setSuggestion([...suggestion, newSuggestion]);
+            e.target.suggestion.value = '';//empties text box after submission
+        }
     };
+
     return(
         <div>
-            <label htmlFor="suggest">Suggestion:</label>
-                <input
-                    type="text"
-                    value={suggestion}
-                    onChange={(e) => setSuggestion(e.target.value)}
-                />
-            <button type="submit" onClick={saveSuggest}>Suggestion</button>
-
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="suggestion">Suggestion:</label>
+                <input type="text" name="suggestion"/>
+                <button type="submit">Add Suggestion</button>
+            </form>
+            <ul>
+                {suggestion.map((suggestion, index) => (
+                    <li key={index}>{suggestion}</li>//Show all suggestions
+                ))}
+            </ul>
         </div>
     );
 };
